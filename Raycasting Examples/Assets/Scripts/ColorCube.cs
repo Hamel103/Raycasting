80b3c8a5392d8ff5f73873bcf1b53f1e4b2e1a25
HamelA <40329125+HamelA@users.noreply.github.com>
2020-05-08 12:54:10 -0700

    Finished three basic use cases
    Messed up and didnt push any inbetween steps.
    
    Contains examples of:
    - Object following mouse
    - Object rotating to match angle of what's below it
    - Object changing color when behind an obstacle and out of sight of a separate object

---
+++ Raycasting Examples/Assets/Scripts/ColorCube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class ColorCube : MonoBehaviour
+{
+    // VARIABLES
+
+    public Transform cube;
+    public LayerMask maskOne;
+    public LayerMask maskTwo;
+
+
+    // METHODS
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = cube.position - transform.position;
+        CubeColor(direction);
+    }
+
+    private void CubeColor(Vector3 direction)
+    {
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, 1000, maskTwo))
+        {
+            cube.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else if (Physics.Raycast(ray, out hitInfo, 1000, maskOne))
+        {
+            hitInfo.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        
+    }
+}
---
+++ Raycasting Examples/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseFollow : MonoBehaviour
+{
+    // VARIABLES
+
+    public Transform objectFollowing;
+    public Camera sceneCamera;
+    
+    public LayerMask mask;
+
+
+    // METHODS
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+    }
+
+    private void FollowMouse()
+    {
+        Ray ray = sceneCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 1000, mask))
+        {
+            objectFollowing.position = hitInfo.point;
+            objectFollowing.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+
+    
+}
